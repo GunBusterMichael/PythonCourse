f4f2c6191062b38c1c356ce1adf319f5c6151030
Honor <dengyufei36@live.com>
2023-06-27 16:29:52 +0800

    添加007笔记

--- Reptile/006.input和类型转换函数.py
+++ Reptile/006.input和类型转换函数.py
@@ -1,4 +1,4 @@
-# userAge = int(input("请输入您的年龄：")) # input 函数默认将获取的数据返回成 str
+userAge = int(input("请输入您的年龄：")) # input 函数默认将获取的数据返回成 str
 print(int('1')) # int 函数可将整数型字符串转换成整数，其他类型字符串会报错
 
 # 整数不能和字符串一起打印
---
+++ Reptile/007.if else判断.py
@@ -0,0 +1,5 @@
+a = int(input('请输入数字：'))
+if a > 50:
+    print('您输入的数字大于50')
+else:
+    print('小于50')
